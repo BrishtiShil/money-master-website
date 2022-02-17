c687b9fe035ea081f99b457ddc0992658751fe2f
Brishti Shil <brishtishil62@gmail.com>
2022-02-17 16:00:42 +0600

    third commit

--- app.js
+++ app.js
@@ -3,19 +3,34 @@ document.getElementById('calculate-button').addEventListener('click', function (
     const incomeAmountNumber = incomeInput.value;
     const incomeAmount = parseFloat(incomeAmountNumber);
     console.log(incomeAmount);
+    // clear income value
+    incomeInput.value = "";
+    console.log(incomeAmount);
+
     // foodValue
     const foodInput = document.getElementById('food-input');
     const foodAmountNumber = foodInput.value;
     const foodAmount = parseFloat(foodAmountNumber);
     console.log(foodAmount);
-    // rentValue
+    // clear food value
+    foodInput.value = "";
+    console.log(foodAmount);
+
+    // rentValue....
     const rentInput = document.getElementById('rent-input');
     const rentAmountNumber = rentInput.value;
     const rentAmount = parseFloat(rentAmountNumber);
     console.log(rentAmount);
-    // clothesValue
+    // clear rent value
+    rentInput.value = "";
+    console.log(rentAmount);
+
+    // clothesValue......
     const clothesInput = document.getElementById('clothes-input');
     const clothesAmountNumber = clothesInput.value;
     const clothesAmount = parseFloat(clothesAmountNumber);
+    console.log(clothesAmount);
+    // clear clothes value
+    clothesInput.value = "";
     console.log(clothesAmount);
 })
