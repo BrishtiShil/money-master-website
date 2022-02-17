853c9ea389945f5b4a04a0f69f6e5c313dcaec90
Brishti Shil <brishtishil62@gmail.com>
2022-02-17 18:59:14 +0600

    sixth commit

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-document.getElementById('calculate-button').addEventListener('click', function () {
+function getInputValue() {
     const incomeInput = document.getElementById('income-input');
     const incomeAmountNumber = incomeInput.value;
     const incomeAmount = parseFloat(incomeAmountNumber);
-    console.log(incomeAmount);
+
     // clear income value
     incomeInput.value = "";
+    return incomeAmount;
+}
 
-    // foodValue
+function getTotalValue() {
     const foodInput = document.getElementById('food-input');
     const foodAmountNumber = foodInput.value;
     const foodAmount = parseFloat(foodAmountNumber);
-    console.log(foodAmount);
+
     // clear food value
     foodInput.value = "";
+    return foodAmount;
+}
+document.getElementById('calculate-button').addEventListener('click', function () {
+    // incomeValue
+    const incomeAmount = getInputValue();
+    // foodValue.....
+    const foodAmount = getTotalValue();
 
     // rentValue....
     const rentInput = document.getElementById('rent-input');
